3313e1b751701562f083d83e26331eac861a1a2a
abl241 <alexanderblee123@gmail.com>
2025-11-11 15:42:01 -0500

    Front end logic for session refreshes.

---
+++ client/src/api/axios.js
@@ -0,0 +1,76 @@
+import axios from "axios";
+
+const api = axios.create({
+    baseURL: "http://localhost:5000",
+    withCredentials: true,
+});
+
+api.interceptors.request.use((config) => {
+    const token = localStorage.getItem("accessToken");
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+});
+
+
+let isRefreshing = false;
+let failedQueue = [];
+
+const processQueue = (error, token = null) => {
+    failedQueue.forEach(prom => {
+        if (error) {
+            prom.reject(error);
+        } else {
+            prom.resolve(token);
+        }
+    });
+    
+    failedQueue = [];
+};
+
+api.interceptors.response.use(
+    (response) => response,
+    async (error) => {
+        const originalRequest = error.config;
+        
+        if (error.response?.status === 401 && !originalRequest._retry) {
+            if (isRefreshing) {
+                return new Promise(function(resolve, reject) {
+                    failedQueue.push({ resolve, reject });
+                }
+            ).then((token) => {
+                originalRequest.headers.Authorization = `Bearer ${token}`;
+                return axios(originalRequest);
+            }).catch((err) => {
+                return Promise.reject(err);
+            });
+        }
+        
+        originalRequest._retry = true;
+        isRefreshing = true;
+        
+        try {
+            const res = await axios.post("http://localhost:5000/auth/refresh",
+                {},
+                { withCredentials: true }
+            );
+            
+            const newToken = res.data.accessToken;
+            localStorage.setItem("accessToken", newToken);
+            api.defaults.headers.common.Authorization = `Bearer ${newToken}`;
+            processQueue(null, newToken);
+            return await api(originalRequest);
+        } catch(err) {
+            processQueue(err, null);
+            localStorage.removeItem("accessToken");
+            window.location.href = "/auth/login";
+            return Promise.reject(err);
+        } finally {
+            isRefreshing = false;
+        }
+    }
+    return Promise.reject(error);
+});
+
+export default api;
